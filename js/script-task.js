7688a53ad07d37df2428b5f17d14d1f569218c07
samanthafav <samanthafavreau@me.com>
2020-09-23 13:58:11 -0700

    Add original files via upload

---
+++ js/script-task.js
@@ -0,0 +1,3 @@
+var taskPrompt = "Enter the task:";
+var task = window.prompt("Enter the task:");
+document.getElementById("task").innerHTML = task;
---
+++ js/script-timer.js
@@ -0,0 +1,18 @@
+
+var duration = .1;
+var now = new Date();
+var deadline = new Date(now.getTime() + duration * 60000);
+var x = setInterval(function () {
+    var flashColor = "Red"
+    var now = new Date().getTime();
+    var t = deadline - now;
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}, 1000); 
