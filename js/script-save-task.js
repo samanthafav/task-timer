f3f11b6fc1451d949362c5063838b8e559052483
samanthafav <samanthafavreau@me.com>
2020-09-29 11:58:28 -0700

    fixed-save-btn

--- js/script-save-task.js
+++ js/script-save-task.js
@@ -17,24 +17,50 @@ function saveTask() {
         clearInputs();
         //add task to visual list
         var para = document.createElement("p");
-        para.setAttribute("id", ("pt-" + nowCount), "onclick", alert("adding onclick to taskname?"))
-        var paraText = (data);
-        para.appendChild(document.createTextNode(paraText));
-        var paraElement = document.getElementById("mainList");
-        paraElement.appendChild(para);
+        para.id = ("pt-" + nowCount);
+        para.innerHTML = data;
+        var Boo = function () {
+            alert("boo");
+        };
+        newBtn = document.createElement("button");
+        newBtn.innerHTML = "Startttttt";
+        newBtn.addEventListener("click", Boo)
+        newBtn.id = ("btn-" + nowCount);
+        para.appendChild(newBtn);
+
+        document.body.appendChild(para);
+
+        document.getElementById("btn-" + nowCount).onclick = function () {
+            Boo;
+        };
+
+
+        //para.setAttribute("id", ("pt-" + nowCount));
+        //var newTaskId = ("pt-" + nowCount);
+        //var paraText = (data);
+        //para.appendChild(document.createTextNode(paraText));
+        //var paraElement = document.getElementById("mainList");
+        //paraElement.appendChild(para);
+
         //add button to each item in visual list
-        var taskBtn = document.createElement("input");
-        taskBtn.setAttribute("type", "button");
-        taskBtn.setAttribute("id", ("tbtn-" + nowCount));
-        taskBtn.setAttribute("value", "start");
+        //var taskBtn = document.createElement("button");
+        //taskBtn.innerHTML = "Start Task!";
+        //document.body.appendChild(taskBtn)
+        //document.getElementById(newTaskId).onclick = "alert("againn")";
+
+
+        //add button to each item in visual list - original idea
+        //var taskBtn = document.createElement("input");
+        //taskBtn.setAttribute("type", "button");
+        //taskBtn.setAttribute("id", ("tbtn-" + nowCount));
+        //taskBtn.setAttribute("value", "start");
         ///taskBtn.setAttribute("onclick", alert("this works?"));
         //taskBtn.innerHTML = "Start";
 
-
-        var taskBtnElement = document.getElementById(("pt-" + nowCount));
-        taskBtnElement.appendChild(taskBtn);
-        var clickAction = alert("maybe this?");
-        taskBtn.setAttribute("onclick", clickAction);
+        //var taskBtnElement = document.getElementById(("pt-" + nowCount));
+        //taskBtnElement.appendChild(taskBtn);
+        //var clickAction = alert("maybe this?");
+        //taskBtn.setAttribute("onclick", clickAction);
 
         //// I'm unable to get the setAttribute to apply the onclick action to the new button i'm making, instead it's just triggered when I run this script.???????
     }
